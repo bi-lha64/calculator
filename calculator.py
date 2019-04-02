03548bd8bdc8384001a6e24959db14d5c6687472
wanjiku <wanjikubilha64@gmail.com>
2019-04-02 21:51:20 +0300

    new file

---
+++ calculator.py
@@ -0,0 +1,30 @@
+def add(a,b):
+	c=a+b
+	print(c)
+
+def multiply(b,t):
+	f=b*t
+	print(f)
+
+def subtraction(q,t):
+	k=q-t
+	print(k)
+
+def divide(l,m):
+	j=l/m
+	print(j)
+
+def modulus(d,h):
+	p=d%h
+	print(p)
+def whole_number_division(s,g):
+	q=s//g
+	print(q)
+
+def exponent(n,b):
+ 	a=n**b
+ 	print(a)
+
+def percentage(n,b):
+ 	a=n*100/b
+ 	print(a)
